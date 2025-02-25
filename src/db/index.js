2e49bd9e34aabce74835c8b88f8f8b9f125eca71
Yogesh Namdev <namdev.yash10@gmail.com>
2025-02-25 22:09:51 +0530

    intial comit

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql2/promise';
+import { DB_NAME } from '../constants.js';
+
+let connectDB;
+
+try {
+  connectDB = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  console.log('Database connection pool created successfully.');
+} catch (error) {
+  console.error('Error creating database connection pool:', error.message);
+  process.exit(1); 
+}
+
+export default connectDB;
